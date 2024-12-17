3612c16ff08a932fb00c9c2951e0815e043bdbb9
Suleyman jama <suleyman.jama@sagacitysoft.ca>
2024-12-17 14:41:48 -0500

    feature: add dark mode and join button refactor: remove react image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,20 +7,21 @@ function App() {
         <div className="hero-content text-center">
           <div className="max-w-2xl">
             <h1 className="text-5xl font-bold">Quiplash Clone</h1>
-            <p className="py-6">
+            <p className="py-6 mb-4">
               The ultimate party game where wit wins the day. Battle friends in
               a hilarious showdown of quick thinking and clever comebacks.
               Answer prompts, vote on the funniest responses, and crown the
               comedy champion! Perfect for game nights, parties, and online
               hangouts. Laughter guaranteed!"
             </p>
-            <button className="btn btn-primary">Star Game</button>
-            <p className="text-2xl font-bold">OR</p>
+            <button className="btn btn-primary mb-4">START GAME</button>
+            <p className="text-2xl font-bold mb-4">OR</p>
             <input
               type="text"
               placeholder="Enter Game Code"
-              className="input input-bordered max-w-xs text-center"
+              className="input input-bordered w-full max-w-[14rem] mr-2"
             />
+            <button className="btn btn-primary">JOIN</button>
           </div>
         </div>
       </div>
