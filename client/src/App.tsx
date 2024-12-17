1dc5b3171d4f9720e0333090e6a7eea3e5abf31c
Ryan Krahn <ryan.krahn@sagacitysoft.ca>
2024-12-17 16:47:54 -0500

    feat: add socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { socket } from "./socket";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    // function onFooEvent(value) {
+    //   setFooEvents(previous => [...previous, value]);
+    // }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    // socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      // socket.off('foo', onFooEvent);
+    };
+  }, []);
+
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const cors = require('cors');
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
 app.use(cors());
 
-app.get('/test', (req, res) => {
-  res.send({some: 'text'});
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+app.get("/test", (req, res) => {
+    res.send({ some: "text" });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log("server running at http://localhost:3000");
 });
