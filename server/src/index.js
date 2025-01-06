5f2d0f6acb2aa3847256c193c9674101d793c5b7
suleyj <suleyj101@gmail.com>
2025-01-06 01:19:05 -0500

    adding old index.js code to src

--- server/src/index.js
+++ server/src/index.js
@@ -21,12 +21,78 @@ app.use(cors());
 const clients = [];
 const rooms = [];
 
+const generateHexCode = (size) => {
+  let result = [];
+  let hexRef = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  for (let n = 0; n < size; n++) {
+    result.push(hexRef[Math.floor(Math.random() * 16)]);
+  }
+  return result.join("").toUpperCase();
+};
+
+const getRoomCode = () => {
+  const codeLength = 5;
+  let code;
+  do {
+    code = generateHexCode(codeLength);
+  } while (rooms.includes(code));
+  return code;
+};
+
 io.on("connection", (socket) => {
   clients.push(socket);
-  socket.on("createRoom", (socket) => {
-    // socket.join(`room-${rooms.length}`);
-    rooms.push(`rooms-${rooms.length}`);
-    console.log(rooms);
+  console.log(`${socket.id} connected`);
+
+  socket.on("createRoom", (callback) => {
+    const roomCode = getRoomCode();
+    socket.join(roomCode);
+    rooms.push({ code: roomCode, players: [] });
+    callback(roomCode);
+  });
+
+  socket.on("joinRoom", (roomCode, callback) => {
+    if (!rooms.some((room) => room.code === roomCode)) {
+      return callback(false);
+    }
+    socket.join(roomCode);
+    callback(true);
+
+    // add new player to the room's player list
+    const currRoom = rooms.find((room) => room.code === roomCode);
+    currRoom.players.push(socket.id);
+    rooms.map((room) => {
+      if (room.code === roomCode) {
+        room = currRoom;
+      }
+    });
+
+    // send the updated player list to all clients in the room
+    io.to(roomCode).emit("playerList", currRoom.players);
+  });
+
+  socket.on("disconnect", () => {
+    //TODO: remove player from room on disconnect
+    console.log(`${socket.id} disconnected`);
+    const index = clients.indexOf(socket);
+    clients.splice(index, 1);
   });
 });
 
